27a177c678297cb26e9000ccc832709dfd43f4b4
DesperateHiking <perevechenkov_1999@mail.ru>
2022-05-25 19:09:02 +0500

    fetching movies from swapi

--- src/App.js
+++ src/App.js
@@ -1,29 +1,36 @@
+import { useState } from 'react';
 import './App.scss';
 import MoviesList from './components/MoviesList';
 
 function App() {
-  const dummyMovies = [
-    {
-      id: 1,
-      title: 'Some Dummy Movie',
-      openingText: 'This is the opening text of the movie',
-      releaseDate: '2021-05-18',
-    },
-    {
-      id: 2,
-      title: 'Some Dummy Movie 2',
-      openingText: 'This is the second opening text of the movie',
-      releaseDate: '2021-05-19',
-    },
-  ];
+  const [movies, setMovies] = useState([]);
+
+  const fetchMoviesHandler = async () => {
+    const responce = await fetch('https://swapi.dev/api/films/');
+
+    if (responce.ok) {
+      const movies = await responce.json();
+
+      const transformedMovies = movies.results.map(movie => {
+        return {
+          id: movie.episode_id,
+          title: movie.title,
+          openingText: movie.opening_crawl,
+          releaseDate: movie.release_date,
+        };
+      });
+
+      setMovies(transformedMovies);
+    }
+  };
 
   return (
     <>
       <section>
-        <button>Fetch Movies</button>
+        <button onClick={fetchMoviesHandler}>Fetch Movies</button>
       </section>
       <section>
-        <MoviesList movies={dummyMovies} />
+        <MoviesList movies={movies} />
       </section>
     </>
   );
