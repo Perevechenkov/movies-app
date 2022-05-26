8af3cf8f33d8db6c02e5f0356f69a2db32fb4dbe
DesperateHiking <perevechenkov_1999@mail.ru>
2022-05-26 19:07:27 +0500

    custom http request hook

---
+++ src/hooks/use-http.js
@@ -0,0 +1,46 @@
+import { useCallback, useState } from 'react';
+
+export default function useHttp(
+  {
+    url,
+    method = 'GET',
+    body = null,
+    headers = {
+      'Content-Type': 'application/json',
+    },
+  },
+  applyData
+) {
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  const sendRequest = useCallback(async () => {
+    setIsLoading(true);
+    setError(null);
+
+    try {
+      const responce = await fetch(url, {
+        method: method,
+        body: body ? JSON.stringify(body) : body,
+        headers: headers,
+      });
+
+      if (!responce.ok) {
+        throw new Error(responce.statusText);
+      }
+
+      const data = await responce.json();
+
+      applyData(data);
+    } catch (error) {
+      setError(error.message);
+    }
+    setIsLoading(false);
+  }, []);
+
+  return {
+    isLoading,
+    error,
+    sendRequest,
+  };
+}
